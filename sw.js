85e988ccb83898330f63d29d0ae5956c2f160bc8
OIPASSS <pazuhindmitrij73@gmail.com>
2026-06-07 00:34:19 +0300

    sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+});
